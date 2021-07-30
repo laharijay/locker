1a0f95a6d1bd7ac99c8e237d84778a903a2499cd
jaylehri <jnlehri@bestpeers.com>
2021-07-30 22:34:51 +0530

    Recoverable Mail Setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -60,8 +60,20 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'personal-locker-app.herokuapp.com', protocol: "https" }
+  config.require_master_key = true
 
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            Rails.application.credentials.dig(:google_smtp, :mail),
+    password:             Rails.application.credentials.dig(:google_smtp, :pass),
+    authentication:       'plain',
+    enable_starttls_auto: true }
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
